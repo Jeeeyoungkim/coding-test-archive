59106cd176a082d75c8580499890520795b4e731
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2024-05-17 16:27:15 +0900

    [level 2] Title: 주식가격, Time: 34.56 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    cnt = len(prices)
+    queue = deque(prices)
+    
+    # index를 기준으로 배열 슬라이싱해서 떨어지는지 찾는다
+    for index, price in enumerate(prices):
+        down = -1
+        queue.popleft()
+        
+        for q in queue:
+            if price > q:
+                down = queue.index(q)
+                break;
+        
+        if down != -1:
+            answer.append(down + 1)
+        else:
+            answer.append(cnt-index-1)
+            
+    return answer
