1d1cf58b1123d90eca6b3084a533923655a369ba
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2024-05-24 13:51:03 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,21 +2,14 @@ import math
 
 def solution(brown, yellow):
     total = brown + yellow
-    possible_set = []
     
     for i in range(1, total+1):
         j = total / i
         if j - int(j) != 0:
             continue
         
-        possible_set.append([i, int(j)])
-    
-    for i, j in possible_set:
-        y_h, y_v = i-2, j-2
-        if y_h * 2 + y_v * 2 + 4 == brown:
+        if (i-2) * 2 + (j-2)* 2 + 4 == brown:
             break;
             
-    if i < j:
-        i, j = j, i
-    answer = [i, j]
+    answer = sorted([i, j], reverse=True)
     return answer
