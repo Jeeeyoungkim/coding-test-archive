f2b8a1b22786ae5c6036cb73be5dcc40918fb356
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2024-05-24 13:52:47 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -4,9 +4,9 @@ def solution(brown, yellow):
     total = brown + yellow
     
     for i in range(1, total+1):
-        j = total / i
-        if j - int(j) != 0:
+        if total % i != 0:
             continue
+        j = int(total / i)
         
         if (i-2) * 2 + (j-2)* 2 + 4 == brown:
             break;
